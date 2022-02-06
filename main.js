34b034a2972eaa62a3c12b88bd9e06d857d86948
Omar <0omarahmed73@gmail.com>
2022-02-06 19:05:07 +0300

    Adding Night Azkar + Fix Some Problems

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const value = document.querySelectorAll(".value");
 console.log(value);
-const set = document.querySelectorAll(".text");
+const set = document.querySelectorAll(".one");
 const reset = document.querySelectorAll(".bi");
 const one  = document.querySelectorAll('.one');
 console.log(one)
@@ -13,12 +13,20 @@ for (let i = 0; i < set.length; i++) {
 });
 }
 
-const dark = document.querySelector('.bi-brightness-alt-high');
-const body = document.querySelector('body');
-console.log(dark)
-dark.addEventListener('click' , () => {
-    body.classList.toggle("dark");
-})
+const morning = document.querySelectorAll('.zikr');
+const night = document.querySelectorAll(".morning")[0];
+const light = document.querySelector(".night");
+console.log(night)
+let display = night.style.display;
+console.log(display)
+morning.forEach(currentItem => {
+    currentItem.addEventListener("click" , (eo) => {
+        light.classList.toggle("show");
+        night.classList.toggle("hide");
+    
+    })  
+});
+
 
 // const fun = () => {
 // for (let i = 0; i < set.length; i++) {
@@ -34,10 +42,15 @@ dark.addEventListener('click' , () => {
 
         for (let i = 0; i < set.length; i++) {
             one[i].addEventListener("click", () => {
-                if (value[i].innerText == 0) {
-                 one[i].style.display = "none";
-                }
+                // if (value[i].innerText == 0) {
+                //  one[i].style.display = "none";
+                // }
              
-             });
-             }
+                setTimeout(() => {
+                    if (value[i].innerText == 0) 
+                    one[i].style.display = "none";
+                }, 200);
+            });
+            }
+            
     
